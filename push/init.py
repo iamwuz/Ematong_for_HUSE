1d98101dfabcfcf6405dfcdd9701309c5566b966
Wu Zhiqiang <124809270+iamwuz@users.noreply.github.com>
2024-09-19 22:52:23 +0800

    Update init.py

--- push/init.py
+++ push/init.py
@@ -4,9 +4,8 @@ from datetime import datetime, timedelta
 
 # 获取推送通知access_token
 def getAT(huse):
-    corpid = 'ww9bb7df2a3f29e83b'  # 企业ID
-    corpsecret = 'pXhUueoywiLGHo-D1bzGXJkEZEUxC76rNDTGcQ1j_J8'  # Secret
-    #corpsecret = 'ehn2EvI1nFVN7iLlTWub6So1siXhWzQjSTmeepNbSmQ'
+    corpid = ''  # 企业ID
+    corpsecret = ''  # Secret
     url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s' % (corpid, corpsecret)
     resp = requests.get(url).json()
     return resp['access_token']
